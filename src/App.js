1986119e22100f919a2bef9321b518a3c68d0697
AnastasiiaPirus <a.pirus2000@gmail.com>
2023-02-26 17:19:14 -0500

    Add router.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Board from './components/board';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <Board />
+    <Router>
+      <Routes>
+        <Route path="/Kanban-dashboard" element={<Board />} exact />         
+      </Routes>
+    </Router>
     </>
   );
 }
